61a69c54f48dd6ba8a9055f15de489fa39156e54
Eric Muyser <eric@muyser.com>
2012-03-09 00:22:16 -0800

    minor fix noConfig option. add construction test. fix #167.

---
+++ test/siblings/construction-test.js
@@ -0,0 +1,33 @@
+/*
+ * test-basic-test.js: Basic tests for the hook.io module
+ *
+ * (C) 2011 Marak Squires, Charlie Robbins
+ * MIT LICENCE
+ *
+ */
+
+var vows = require('vows'),
+    assert = require('assert'),
+    Hook = require('../../lib/hookio').Hook,
+    macros = require('../helpers/macros');
+
+vows.describe('hook.io/spawn/construction').addBatch({
+  "When hook is constructed with no arguments": {
+    topic: function() {
+      var hook = new Hook();
+
+      this.callback();
+    },
+    "it should succeed": function(a, b) { }
+  },
+  "When hook is constructed with no config": {
+    topic: function() {
+      var hook = new Hook({
+        noConfig: true
+      });
+
+      this.callback();
+    },
+    "it should succeed": function(a, b) { }
+  }
+}).export(module);
